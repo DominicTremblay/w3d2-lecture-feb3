b940e85e29b184720fd35994032a50ad88482571
Dominic Tremblay <dominictremblay24@gmail.com>
2020-02-17 22:04:37 -0500

    shorten the ids in the db

--- server.js
+++ server.js
@@ -16,33 +16,33 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 const movieQuotesDb = {
-  'd9424e04-9df6-4b76-86cc-9069ca8ee4bb': {
-    id: 'd9424e04-9df6-4b76-86cc-9069ca8ee4bb',
+  d9424e04: {
+    id: 'd9424e04',
     quote: 'Why so serious?',
   },
-  '27b03e95-27d3-4ad1-9781-f4556c1dee3e': {
-    id: '27b03e95-27d3-4ad1-9781-f4556c1dee3e',
+  '27b03e95': {
+    id: '27b03e95',
     quote: 'YOU SHALL NOT PASS!',
   },
-  '5b2cdbcb-7b77-4b23-939f-5096300e1100': {
-    id: '5b2cdbcb-7b77-4b23-939f-5096300e1100',
+  '5b2cdbcb': {
+    id: '5b2cdbcb',
     quote: "It's called a hustle, sweetheart.",
   },
-  '917d445c-e8ae-4ed9-8609-4bf305de8ba8': {
-    id: '917d445c-e8ae-4ed9-8609-4bf305de8ba8',
+  '917d445c': {
+    id: '917d445c',
     quote: 'The greatest teacher, failure is.',
   },
-  '4ad11feb-a76a-42ae-a1c6-8e30dc12c3fe': {
-    id: '4ad11feb-a76a-42ae-a1c6-8e30dc12c3fe',
+  '4ad11feb': {
+    id: '4ad11feb',
     quote: 'Speak Friend and Enter',
   },
 };
 
 const quoteComments = {
-  '70fcf8bd-6cb0-42f3-9887-77aa9db4f0ac': {
-    id: '70fcf8bd-6cb0-42f3-9887-77aa9db4f0ac',
+  '70fcf8bd': {
+    id: '70fcf8bd',
     comment: 'So awesome comment!',
-    quoteId: 'd9424e04-9df6-4b76-86cc-9069ca8ee4bb',
+    quoteId: 'd9424e04',
   },
 };
 
