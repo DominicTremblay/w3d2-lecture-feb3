2538aa6e33af5e903f33525b4541bc2d228f85f9
Dominic Tremblay <dominictremblay24@gmail.com>
2020-02-18 14:21:15 -0500

    final demo feb 3

--- server.js
+++ server.js
@@ -46,8 +46,130 @@ const quoteComments = {
   },
 };
 
+// Returns an array of quote objects with the corresponding comments
+const quoteList = () => {
+  // Making the object an array
+  const quotesArr = Object.values(movieQuotesDb);
+  const commentsArr = Object.values(quoteComments);
+
+  // map loops and transform
+  const quoteListComments = quotesArr.map(quote => {
+    quote.comments = commentsArr.filter(comment => {
+      return quote.id === comment.quoteId;
+    });
+
+    return quote;
+  });
+
+  return quoteListComments;
+};
+
+// Create a new quote Object and add it to the db
+const addNewQuote = quoteContent => {
+  // generating a random id for our new quote
+  const quoteId = uuid().substr(0, 8);
+
+  const newQuote = {
+    id: quoteId,
+    quote: quoteContent,
+  };
+
+  // Add it to movieQuotesDb
+  // object name + [key] = value
+
+  movieQuotesDb[quoteId] = newQuote;
+  return quoteId;
+  b;
+};
+// End points for our app
+
+// list of quotes
+// http Verb (method): GET
+// path: '/quotes'/
+// req -> request object
+// res -> response object
 app.get('/quotes', (req, res) => {
-  res.render('quotes');
+  // console.log('method', req.method);
+  // console.log('path', req.url);
+  // console.log('headers', req.headers);
+
+  // get the list of quotes + comments from our db object
+
+  const quotes = quoteList();
+
+  // render the quotes HTML page
+
+  res.render('quotes', { quoteList: quotes });
+});
+
+// Add a new quote
+// method: GET
+// path: '/quotes/new'
+
+app.get('/quotes/new', (req, res) => {
+  // render the new form
+
+  res.render('new_quote');
+});
+
+// When the user clicks on the submit of the form
+// method: POST
+// path: '/quotes'
+app.post('/quotes', (req, res) => {
+  // Get the quote content from the input form
+  // This requires a middleware called bodyParser
+  const quoteContent = req.body.quoteContent;
+
+  addNewQuote(quoteContent);
+
+  // redirect to /quotes page
+
+  res.redirect('/quotes');
+});
+
+// Deleting a quote
+// Method: should be delete, but forms don't understand delete. So POST instead.
+// path: /quotes/:id/delete
+// :id is a wildcard
+
+app.post('/quotes/:id/delete', (req, res) => {
+  // extract the id value from the path with req.params
+  const quoteId = req.params.id;
+
+  // delete the object with that id in the db
+  delete movieQuotesDb[quoteId];
+
+  // redirect to /quotes
+  res.redirect('/quotes');
+});
+
+// display the form to see one quote
+// method: GET
+// path: '/quotes/:id/update
+
+app.get('/quotes/:id/update', (req, res) => {
+  // extract the id from the url
+  const quoteId = req.params.id;
+  // get the quote from db
+  const movieQuote = movieQuotesDb[quoteId];
+
+  const templateVar = { quoteObj: movieQuote };
+
+  res.render('quote_show', templateVar);
+});
+
+app.post('/quotes/:id/update', (req, res) => {
+  // extract the id from the url
+  const quoteId = req.params.id;
+
+  //extract the quote content from the form
+  const quoteContent = req.body.quoteContent;
+
+  // update the content of the quote for that particular quote in the db
+
+  movieQuotesDb[quoteId].quote = quoteContent;
+
+  res.redirect('/quotes');
 });
 
 app.listen(PORT, () => {
